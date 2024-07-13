d3cd0a620a70785309baf2df0965e40f9644d091
Ananthu AP <ananthuap48@gmail.com>
2024-07-13 16:40:17 +0530

    fixagain0

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.get("/",(req,res)=>{
-    res.json({message: "hiii"})
+    res.json({message: "this is backend"})
 })
 
 app.use("/api/users", userRoutes);
