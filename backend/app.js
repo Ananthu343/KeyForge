9888b624e91388c6efd4c91a22d5c435205d66a0
Ananthu AP <ananthuap48@gmail.com>
2024-07-13 15:07:19 +0530

    vercel config

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,9 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.get("/",(req,res)=>{
+    res.json({message: "hiii"})
+})
 
 app.use("/api/users", userRoutes);
 
